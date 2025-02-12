02883e07f20dc64d9247fdebce93c870f0f0b30d
chanhtv235 <chanhtv235@gmail.com>
2025-02-12 20:15:38 +0700

    ss3_thao tác csdl

--- database/ss2_thiet_ke_csdl.sql
+++ database/ss2_thiet_ke_csdl.sql
@@ -1,7 +1,7 @@
 create database if not exists m3_c0924l1_jv103;
 use m3_c0924l1_jv103;
 create table jame (
- `account` varchar(50) primary key,
+ `username` varchar(50) primary key,
  `password` varchar(50)
 );
 create table class (
@@ -21,9 +21,9 @@ create table student(
  birthday date,
  email varchar(100),
  point float,
- `account` varchar(50) unique,
+ `username` varchar(50) unique,
  class_id int,
- foreign key (`account`) references jame(`account`),
+ foreign key (`username`) references jame(`username`),
  foreign key (class_id) references class(id)
  );
  
@@ -41,6 +41,100 @@ create table instructor (
   primary key (instructor_id,class_id),
   foreign key (instructor_id) references instructor(id),
   foreign key (class_id) references class(id)
- )
+ );
  
+ -- input data
+insert into class (name) values ('c1121g1'), ('c1221g1'),('a0821i1'),('a0921i1');
+insert into room(name,class_id) values ('Ken',1), ('Jame',1),('Ada',2),('Andy',2);
 
+insert into jame(`username`,`password`)
+ values('cunn','12345'),('chunglh','12345'),('hoanhh','12345'),('dungd','12345'),('huynhtd','12345'),
+ ('hainm','12345'),('namtv','12345'),('hieuvm','12345'),('kynx','12345'),('vulm','12345');
+
+insert into jame(`username`,`password`)
+ values('chau','12345');
+ 
+insert into instructor(`name`,birthday, salary)
+ values('tran van chanh','1985-02-03',100),('tran minh chien','1985-02-03',200),('vu thanh tien','1985-02-03',300);
+insert into instructor(`name`,birthday, salary)
+ values('tran van nam','1989-12-12',100);
+
+
+ 
+ insert into student(`name`,birthday, gender,`point`, class_id,`username`) 
+ values ('nguyen ngoc cu','1981-12-12',1,8,1,'cunn'),('le hai chung','1981-12-12',1,5,1,'chunglh'),
+ ('hoang huu hoan','1990-12-12',1,6,2,'hoanhh'),('dau dung','1987-12-12',1,8,1,'dungd'),
+ ('ta dinh huynh','1981-12-12',1,7,2,'huynhtd'),('nguyen minh hai','1987-12-12',1,9,1,'hainm'),
+ ('tran van nam','1989-12-12',1,4,2,'namtv'),('vo minh hieu','1981-12-12',1,3,1,'hieuvm'),
+ ('le xuan ky','1981-12-12',1,7,2,'kynx'),('le minh vu','1981-12-12',1,7,1,'vulm');
+
+  insert into student(`name`,birthday, gender,`point`, class_id) 
+ values ('nguyen van a','1981-12-12',1,8,null),('tran van b','1981-12-12',1,5,null);
+ 
+--   insert into student(`name`,birthday, gender,`point`, class_id,`username`) 
+--  values ('nguyen minh hai chau','1981-12-12',1,8,null,'chau');
+ 
+ insert into instructor_class(class_id,instructor_id) values (1,1),(1,2),(2,1),(2,2),(3,1),(3,2);
+
+select * from room;
+select* from class;
+select* from student;
+select* from jame;
+select* from instructor;
+select * from instructor_class;
+
+-- bài 3 thao tác với csdl
+
+-- Câu 1. Lấy ra thông tin các học viên, và cho biết các học viên đang theo học lớp nào? --
+select s.*, c.name as class_name from student  s
+join class  c on s.class_id = c.id ;
+
+-- Câu 2. Lấy ra thông tin các học viên, và cho biết các học viên đang theo học lớp nào 
+-- và cả các bạn đã đăng ký nhưng chưa có lớp học? --
+select s.*, c.name as class_name from student  s
+right join class  c on s.class_id = c.id;
+
+--  Câu 3. Lấy thông tin của các học viên tên 'nguyen minh hai'?
+select * from student where name = 'nguyen minh hai';
+  
+-- Câu 4. Lấy ra học viên có họ là “nguyen”
+select * from student where name like 'nguyen %';
+
+--  Câu 5. Lấy thông tin của các học viên tên 'hai' hoặc 'huynh’.
+select * from student where name like '% hai' or name like '% huynh';
+
+--      Câu 6. Lấy ra các học viên có điểm lớn hơn 5
+select * from student where point >5;
+
+-- Câu 7. lấy ra thông tin các học viên có điểm 4,6,8
+select * from student where point in (4,6,8);
+
+--  lấy ra các loại điểm trong bảng student; 
+select distinct point from student;
+-- Câu 8. Thông kế số lượng học sinh theo từng loại điểm.
+     
+select point, count(point) as so_luong from student group by point;
+      
+-- Câu 9 . Thông kế số lượng học sinh theo điểm và điểm phải lớn hơn =5
+
+   select point, count(point) as so_luong from student where point>=5 group by point;
+   
+-- Câu 10. Thông kế số lượng học sinh theo điểm lớn hơn 5 và chỉ hiện thị với số lượng>=2
+
+   select point, count(point) as so_luong from student where point>=5 group by point having so_luong>=2;
+   
+-- tìm hiểu khác nhau giữa where và having ( having có thể thay thể cho where được không? và ngược lại)
+
+-- Câu 11. Lấy ra danh sách học viên của lớp c1121g1 và sắp xếp  học viên giảm dần theo point,
+-- nếu bằng tuổi thì sắp xếp tăng dần theo tên
+
+ select * from student s
+ join class c on s.class_id = c.id
+ where c.name ='c1121g1' order by s.point desc, s.name asc;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
