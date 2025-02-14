58eeb013fd287260c704a62334a1082ab243c7b1
chanhtv235 <chanhtv235@gmail.com>
2025-02-14 20:19:31 +0700

    ss4

--- database/ss2_thiet_ke_csdl.sql
+++ database/ss2_thiet_ke_csdl.sql
@@ -133,6 +133,69 @@ select point, count(point) as so_luong from student group by point;
  where c.name ='c1121g1' order by s.point desc, s.name asc;
  
  
+ -- bài 4: sử dụng các hàm thông dụng
+ select* from class;
+select* from student;
+-- tính số lượng học viên của trung tâm: => count
+select count(id) as so_luong from student;
+-- 1.1	Hiện thị danh sách các lớp có học viên theo học và số lượng học viên của mỗi lớp
+select c.name as class_name, count(s.id) as so_luong from class c
+join student s on s.class_id = c.id 
+group by c.id;
+-- 1.2.	 Tính điểm lớn nhất của mỗi các lớp
+select c.name as class_name, max(s.point) as max_point from class c
+join student s on s.class_id = c.id 
+group by c.id;
+
+-- 1.3	 Tình điểm trung bình  của từng lớp
+select c.name as class_name, round(avg(s.point),2) as avg_point from class c
+join student s on s.class_id = c.id 
+group by c.id;
+
+-- 2 Lấy ra toàn bộ tên và ngày sinh các instructor và student ở CodeGym.
+
+select name, point from student
+union all
+select name, birthday from instructor;
+
+-- thực hiện full join cho student và class
+select *  from class c
+left join student s on s.class_id = c.id 
+union
+select *  from class c
+right join student s on s.class_id = c.id ;
+
+-- 4 Lấy ra top 3 học viên có điểm cao nhất của trung tâm.
+select * from student order by point desc limit 0, 3;
+
+-- 3 record tiếp the
+select * from student order by point desc limit 3, 3;
+
+select * from student;
+
+-- 5. Lấy ra các học viên có điểm số là cao nhất của trung tâm.
+
+select * from student where point = (select max(point) from student);
+
+
+-- 6. lấy ra tất cả các giảng viên chưa từng tham gia giảng dạy
+
+-- join bảng : khuyến khích dùng
+select * from instructor i 
+left join instructor_class ic on i.id=ic.instructor_id
+where ic.instructor_id is null;
+
+-- câu truy vấn con: không khuyến khích
+
+select * from instructor i
+ where id not in (
+    select distinct instructor_id from instructor_class
+ );
+
+
+
+ 
+ 
  
  
  
